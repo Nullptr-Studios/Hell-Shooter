ea3b81899f1ca13854442efe09a8c25953c1158c
Xein <xgonip@gmail.com>
2024-09-21 17:25:43 +0200

    Added acceleration

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,32 +1,33 @@
-using System;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D m_Rigidbody;
-    private Vector2 m_Direction;
-    [NonSerialized] public bool IsMoving = false;
-    public float maxSpeed = 4f;
-    [SerializeField] private float m_CurrentSpeed = 0f;
+    // Private variables
+    private Rigidbody2D _rb;
+    private Vector2 _direction;
+    private float _accelTimer = 0;
     
-    // Acceleration variables
-    public AnimationCurve accelerationCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
-    public AnimationCurve decelerationCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
-    public float accelerationTime = 0.5f;
-    public float decelerationTime = 0.5f;
+    // Public variables
+    [SerializeField] [Range(1f, 8f)] private float maxSpeed;
+    [SerializeField] [ReadOnly] private float currentSpeed;
+    [Header("Acceleration variables")]
+    [SerializeField] private float accelerationTime;
+    // [SerializeField] private float decelerationTime;
     
     // Start is called before the first frame update
     void Start()
     {
-        m_Rigidbody = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        IsMoving = m_Rigidbody.velocity != Vector2.zero;
-        m_Rigidbody.velocity = m_Direction * maxSpeed;
+        _rb.velocity = maxSpeed * Vector2.Lerp(Vector2.zero, _direction, _accelTimer);
+        currentSpeed = _rb.velocity.magnitude;
+        if (_accelTimer < 1) _accelTimer += Time.deltaTime / accelerationTime; 
     }
 
     /**
@@ -36,6 +37,8 @@ public class PlayerMovement : MonoBehaviour
      */
     private void OnMove(InputValue value)
     {
-        m_Direction = value.Get<Vector2>();
+        if (value.Get<Vector2>() != Vector2.zero && _direction == Vector2.zero)
+            _accelTimer = 0;
+        _direction = value.Get<Vector2>();
     }
 }
