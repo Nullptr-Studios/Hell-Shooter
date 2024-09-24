62e4721edd58c2cb21cbe62d5dac2bc17252b4dc
Xein <xgonip@gmail.com>
2024-09-24 20:35:02 +0200

    Give XP when enemy dies

--- Assets/Scripts/EnemyHealthSystem.cs
+++ Assets/Scripts/EnemyHealthSystem.cs
@@ -7,6 +7,8 @@ public class EnemyHealthSystem : MonoBehaviour
     public float maxHealth = 100.0f;
     public float currentHealth;
     public float criticalHitMultiplier = 2.0f;
+    public int killedXp = 10;
+    // public int killedGold = 2; prepared for when gold is used
     
     // Start is called before the first frame update
     void Awake()
@@ -41,6 +43,7 @@ public class EnemyHealthSystem : MonoBehaviour
         {
             Debug.Log("I'm Ded");
             currentHealth = 0;
+            playerStats.GiveXP(killedXp);
             //Idunno if unity has something like delegates or event notifies in Unreal, in order to keep count of dead enemies and it's score
             //@TODO:Do something fancy (particles...etc)
             Destroy(this.gameObject);
