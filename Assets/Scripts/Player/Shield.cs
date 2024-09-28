01f9e2b1aa77710a2c817ca6868e895cbda12b41
Xein <xgonip@gmail.com>
2024-09-29 01:08:13 +0200

    Started to work on shield mechanic

--- Assets/Scripts/Player/Shield.cs
+++ Assets/Scripts/Player/Shield.cs
@@ -1,11 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Shield : MonoBehaviour
 {
+    public int maxHealth = 1;
+    private int _currentHealth;
+    public float cooldownTime;
+    private float _cooldownStart;
+    private bool _onCooldown;
+    private bool _shieldActive;
+
+    /// <summary>
+    /// Current health the shield has.
+    /// Read only.
+    /// </summary>
+    public int health
+    {
+        get => _currentHealth; 
+    }
+
+    /// <summary>
+    /// Returns true if Shield cooldown is active and cannot be used.
+    /// Read only.
+    /// </summary>
+    public bool cooldownActive
+    {
+        get => _onCooldown;
+    }
+
+    /// <summary>
+    /// Returns true if Shield is active at the moment.
+    /// Read only.
+    /// </summary>
+    public bool isActive
+    {
+        get => _shieldActive;
+    }
+
+    [Header("Visual Components")]
     public SpriteRenderer shieldRenderer;
     public Collider2D shieldCollider;
     
@@ -24,7 +56,27 @@ public class Shield : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Cooldown timer end check
+        if (Time.time - _cooldownStart > cooldownTime)
+        {
+            _currentHealth = maxHealth;
+            _onCooldown = false;
+        }
+    }
+
+    /// <summary>
+    /// Removes one health point from the shield and checks for low health
+    /// </summary>
+    public void DoDamage()
+    {
+        _currentHealth--;
         
+        if (_currentHealth <= 0)
+        {
+            _cooldownStart = Time.time;
+            _onCooldown = true;
+            _shieldActive = false;
+        }
     }
 
     public void OnShield(InputAction.CallbackContext context)
@@ -32,14 +84,21 @@ public class Shield : MonoBehaviour
         if (context.action.name != "Shield")
             return;
         
+        if (_onCooldown)
+            return;
+        
         if (context.performed)
         {
+            _shieldActive = true;
+            
             shieldRenderer.enabled = true;
             shieldCollider.enabled = true;
         }
 
         if (context.canceled)
         {
+            _shieldActive = false;
+            
             shieldRenderer.enabled = false;
             shieldCollider.enabled = false;
         }
