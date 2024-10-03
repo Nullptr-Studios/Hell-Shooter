8e02e6503cc12095d641e6d1acb6c58c60769f44
Xein <xgonip@gmail.com>
2024-10-03 18:15:27 +0200

    Added classes

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,7 +1,9 @@
+using System;
 using TMPro;
 using ToolBox.Serialization;
 using UnityEngine;
 using UnityEngine.UI;
+using Object = UnityEngine.Object;
 
 public class Shop : MonoBehaviour
 {
@@ -11,290 +13,145 @@ public class Shop : MonoBehaviour
     public TextMeshProUGUI goldText;
     private int _gold = 99;
 
-    [Header("Dash")] 
-    public GameObject dPrefab;
-    public Button dPucharse;
-    public Button dEquip;
-    public Image dDisabled;
-    public TextMeshProUGUI dGoldText;
-    public int dPrice = 1;
-    private GameObject _dObject;
-    private bool _dBought;
-    private bool _dEquipped;
+    // Object Classes
+    [SerializeField] private ShopItem dash;
+    [SerializeField] private ShopItem shield;
+    //[SerializeField] private ShopItem dash;
     
-    [Header("Shield")] 
-    public GameObject sPrefab;
-    public Button sPucharse;
-    public Button sEquip;
-    public Image sDisabled;
-    public TextMeshProUGUI sGoldText;
-    public int sPrice = 1;
-    private GameObject _sObject;
-    private bool _sBought;
-    private bool _sEquipped;
-    
-    [Header("Health +1")]
-    public GameObject hPrefab;
-    public Button hPucharse;
-    public Slider hSlider;
-    public Image hDisabled;
-    public TextMeshProUGUI hGoldText;
-    public int hPrice = 1;
-    private GameObject _hObject;
-    private bool _hBought;
-    private int _hLevel;
+    [SerializeField] private ShopItem defaultWeapon;
+    [SerializeField] private ShopItem tripleWeapon;
+    [SerializeField] private ShopItem burstWeapon;
 
-    [Header("Triple Weapon")] 
-    public GameObject tPrefab;
-    public Button tPucharse;
-    public Button tEquip;
-    public Image tDisabled;
-    public TextMeshProUGUI tGoldText;
-    public int tPrice = 1;
-    private bool _tBought;
-    private GameObject _tObject;
-    
-    [Header("Burst Weapon")]
-    public GameObject bPrefab;
-    public Button bPucharse;
-    public Button bEquip;
-    public Image bDisabled;
-    public TextMeshProUGUI bGoldText;
-    public int bPrice = 1;
-    private bool _bBought;
-    private GameObject _bObject;
-    
-    [Header("Default Weapon")]
-    public GameObject wPrefab;
-    public Button wEquip;
-    private GameObject _wObject;
-
-    private int _equippedWeapon;
+    public int equippedWeapon;
     private GameObject _player;
 
     private void Awake()
     {
         _player = GameObject.FindWithTag("Player");
+        defaultWeapon.isBought = true;
 
-        _dBought = DataSerializer.Load<bool>(SaveDataKeywords.dashBought);
-        _dEquipped = DataSerializer.Load<bool>(SaveDataKeywords.dashEquipped);
-        _sBought = DataSerializer.Load<bool>(SaveDataKeywords.shieldBought);
-        _sEquipped = DataSerializer.Load<bool>(SaveDataKeywords.shieldEquipped);
-
-        _tBought = DataSerializer.Load<bool>(SaveDataKeywords.tripleBought);
-        _bBought = DataSerializer.Load<bool>(SaveDataKeywords.burstBought);
-        _equippedWeapon = DataSerializer.Load<int>(SaveDataKeywords.weaponEquiped);
-
-        // Check if Dash bought
-        if (!_dBought)
-        {
-            dEquip.interactable = false;
-        }
-        else
-        {
-            dEquip.interactable = true;
-            Destroy(dPucharse.gameObject);
-            Destroy(dDisabled.gameObject);
-            
-            dEquip.GetComponent<Image>().color = _dEquipped? activeColor : inactiveColor;
-            if (_dEquipped) DashInstanciate();
-        }
+        dash.isBought = DataSerializer.Load<bool>(SaveDataKeywords.dashBought);
+        dash.isEquiped = DataSerializer.Load<bool>(SaveDataKeywords.dashEquipped);
+        shield.isBought = DataSerializer.Load<bool>(SaveDataKeywords.shieldBought);
+        shield.isEquiped = DataSerializer.Load<bool>(SaveDataKeywords.shieldEquipped);
         
-        // Check if Shield bought
-        if (!_sBought)
-        {
-            sEquip.interactable = false;
-        }
-        else
-        {
-            sEquip.interactable = true;
-            Destroy(sPucharse.gameObject);
-            Destroy(sDisabled.gameObject);
-            
-            sEquip.GetComponent<Image>().color = _sEquipped? activeColor : inactiveColor;
-            if (_sEquipped) ShieldInstanciate();
-        }
+        tripleWeapon.isBought = DataSerializer.Load<bool>(SaveDataKeywords.tripleBought);
+        burstWeapon.isBought = DataSerializer.Load<bool>(SaveDataKeywords.burstBought);
+        equippedWeapon = DataSerializer.Load<int>(SaveDataKeywords.weaponEquiped);
         
-        // -------- WEAPONS --------------------------------
-        // Check if Triple bought
-        if (!_tBought)
-        {
-            tEquip.interactable = false;
-        }
-        else
-        {
-            tEquip.interactable = true;
-            Destroy(tPucharse.gameObject);
-            Destroy(tDisabled.gameObject);
-            
-            tEquip.GetComponent<Image>().color = _equippedWeapon == 1? activeColor : inactiveColor;
-            if (_equippedWeapon == 1) TripleWInstanciate();
-        }
+        dash.Awake(activeColor, inactiveColor, _player, 0);
+        shield.Awake(activeColor, inactiveColor, _player, 1);
         
-        // Check if Burst bought
-        if (!_bBought)
-        {
-            bEquip.interactable = false;
-        }
-        else
-        {
-            bEquip.interactable = true;
-            Destroy(bPucharse.gameObject);
-            Destroy(bDisabled.gameObject);
-            
-            bEquip.GetComponent<Image>().color = _equippedWeapon == 2? activeColor : inactiveColor;
-            if (_equippedWeapon == 2) BurstWInstanciate();
-        }
-        
-        // Default Elected
-        wEquip.GetComponent<Image>().color = _equippedWeapon == 0? activeColor : inactiveColor;
-        if (_equippedWeapon == 0) DefaultWInstanciate();
+        tripleWeapon.AwakeWeapon(activeColor, inactiveColor, _player, equippedWeapon, 1);
+        burstWeapon.AwakeWeapon(activeColor, inactiveColor, _player, equippedWeapon, 2);
+        defaultWeapon.AwakeWeapon(activeColor, inactiveColor, _player, equippedWeapon, 0);
     }
 
     // Equip functions
     // This functions handle the equip button for each item
     
-    public void DashEquip()
-    {
-        if (!_dEquipped)
-        {
-            _dEquipped = true;
-            dEquip.GetComponent<Image>().color = activeColor;
-            
-            DashInstanciate();
-        }
-        else
-        {
-            _dEquipped = false;
-            dEquip.GetComponent<Image>().color = inactiveColor;
-            Destroy(_dObject);
-        }
-    }
-    
-    public void ShieldEquip()
-    {
-        if (!_sEquipped)
-        {
-            _sEquipped = true;
-            sEquip.GetComponent<Image>().color = activeColor;
-            
-            ShieldInstanciate();
-        }
-        else
-        {
-            _sEquipped = false;
-            sEquip.GetComponent<Image>().color = inactiveColor;
-            Destroy(_sObject);
-        }
-    }
-    
-    public void TripleWEquip()
-    {
-        if (_equippedWeapon != 1)
-        {
-            _equippedWeapon = 1;
-            wEquip.GetComponent<Image>().color = inactiveColor;
-            tEquip.GetComponent<Image>().color = activeColor;
-            bEquip.GetComponent<Image>().color = inactiveColor;
-            
-            Destroy(_wObject);
-            Destroy(_bObject);
-            
-            TripleWInstanciate();
-        }
-    }
+    public void DashEquip() => dash.Equip(activeColor, inactiveColor, _player, 0);
+    public void ShieldEquip() => shield.Equip(activeColor, inactiveColor, _player, 1);
 
-    public void BurstWEquip()
-    {
-        if (_equippedWeapon != 2)
-        {
-            _equippedWeapon = 2;
-            wEquip.GetComponent<Image>().color = inactiveColor;
-            tEquip.GetComponent<Image>().color = inactiveColor;
-            bEquip.GetComponent<Image>().color = activeColor;
-            
-            Destroy(_tObject);
-            Destroy(_wObject);
-            
-            BurstWInstanciate();
-        }
-    }
-    
-    public void DefaultWEquip()
-    {
-        if (_equippedWeapon != 0)
-        {
-            _equippedWeapon = 0;
-            wEquip.GetComponent<Image>().color = activeColor;
-            tEquip.GetComponent<Image>().color = inactiveColor;
-            bEquip.GetComponent<Image>().color = inactiveColor;
-            
-            Destroy(_tObject);
-            Destroy(_bObject);
-            
-            DefaultWInstanciate();
-        }
-    }
-
-
-    // Instantiate functions
-    // Each function instantiates the game object
-    // Cant do classes because can't call function from non-component classes with Unity Events
-    // Unity devs pls cut yourselves harder than I do -x
-    void DashInstanciate()
-    {
-        // Player instanciate prefab
-        _dObject = Instantiate(dPrefab, _player.transform, true);
-        _dObject.GetComponent<Dash>().enabled = false;
-        var tr = _dObject.transform;
-        _dObject.transform.localPosition = Vector3.zero + tr.position;
-        _dObject.transform.localScale = tr.lossyScale;
-        _dObject.transform.localRotation = tr.rotation;
-    }
-    
-    void ShieldInstanciate()
-    {
-        // Player instanciate prefab
-        _sObject = Instantiate(sPrefab, _player.transform, true);
-        _sObject.GetComponent<Shield>().enabled = false;
-        var tr = _sObject.transform;
-        _sObject.transform.localPosition = Vector3.zero + tr.position;
-        _sObject.transform.localScale = tr.lossyScale;
-        _sObject.transform.localRotation = tr.rotation;
-    }
-    
-    void TripleWInstanciate()
-    {
-        // Player instanciate prefab
-        _tObject = Instantiate(tPrefab, _player.transform, true);
-        _tObject.GetComponent<TripleSettings>().enabled = false;
-        var tr = _tObject.transform;
-        _tObject.transform.localPosition = Vector3.zero + tr.position;
-        _tObject.transform.localScale = tr.lossyScale;
-        _tObject.transform.localRotation = tr.rotation;
-    }
-    
-    void BurstWInstanciate()
-    {
-        // Player instanciate prefab
-        _bObject = Instantiate(bPrefab, _player.transform, true);
-        _bObject.GetComponent<BurstSettings>().enabled = false;
-        var tr = _bObject.transform;
-        _bObject.transform.localPosition = Vector3.zero + tr.position;
-        _bObject.transform.localScale = tr.lossyScale;
-        _bObject.transform.localRotation = tr.rotation;
-    }
-    
-    void DefaultWInstanciate()
-    {
-        // Player instanciate prefab
-        _wObject = Instantiate(wPrefab, _player.transform, true);
-        _wObject.GetComponent<DefaultWeapon>().enabled = false;
-        var tr = _wObject.transform;
-        _wObject.transform.localPosition = Vector3.zero + tr.position;
-        _wObject.transform.localScale = tr.lossyScale;
-        _wObject.transform.localRotation = tr.rotation;
-    }
+    public void TripleWEquip() => tripleWeapon.EquipWeapon(activeColor, inactiveColor, _player, 1, this, defaultWeapon, burstWeapon);
+    public void BurstWEquip() => tripleWeapon.EquipWeapon(activeColor, inactiveColor, _player, 2, this, defaultWeapon, tripleWeapon);
+    public void DefaultWEquip() => tripleWeapon.EquipWeapon(activeColor, inactiveColor, _player, 0, this, tripleWeapon, burstWeapon);
 }
 
-// TODO: Try to make this a class FUCK UNITY
+[Serializable] internal class ShopItem
+{
+    public int price = 1;
+    [NonSerialized] public bool isBought = false;
+    [NonSerialized] public bool isEquiped = false;
+    
+    [Header("UI Elements")]
+    public Button pucharseButton;
+    public Button equipButton;
+    public Image disabledImage;
+    public TextMeshProUGUI priceText;
+    public Slider levelSlider;
+
+    [Header("Game Objects")]
+    public GameObject prefab;
+    [NonSerialized] public GameObject obj;
+
+    public void Awake(Color activeColor, Color inactiveColor, GameObject player, int id)
+    {
+        // Check if Dash bought
+        if (!isBought)
+        {
+            equipButton.interactable = false;
+        }
+        else
+        {
+            equipButton.interactable = true;
+            Object.Destroy(pucharseButton.gameObject);
+            Object.Destroy(disabledImage.gameObject);
+            
+            equipButton.GetComponent<Image>().color = isEquiped? activeColor : inactiveColor;
+            if (isEquiped) Create(player, id);
+        }
+    }
+    
+    public void AwakeWeapon(Color activeColor, Color inactiveColor, GameObject player, int equippedWeapon, int weaponID)
+    {
+        if (!isBought)
+        {
+            equipButton.interactable = false;
+        }
+        else
+        {
+            equipButton.interactable = true;
+            Object.Destroy(pucharseButton.gameObject);
+            Object.Destroy(disabledImage.gameObject);
+            
+            equipButton.GetComponent<Image>().color = equippedWeapon == 1? activeColor : inactiveColor;
+            if (equippedWeapon == weaponID) Create(player, -1);
+        }
+    }
+    
+    public void Equip(Color activeColor, Color inactiveColor, GameObject player, int id)
+    {
+        if (!isEquiped)
+        {
+            isEquiped = true;
+            equipButton.GetComponent<Image>().color = activeColor;
+        
+            Create(player, id);
+        }
+        else
+        {
+            isEquiped = false;
+            equipButton.GetComponent<Image>().color = inactiveColor;
+            Object.Destroy(obj);
+        }
+    }
+    
+    public void EquipWeapon(Color activeColor, Color inactiveColor, GameObject player, int weaponID, Shop shop, ShopItem w1, ShopItem w2)
+    {
+        if (shop.equippedWeapon == weaponID) return;
+        shop.equippedWeapon = weaponID;
+            
+        equipButton.GetComponent<Image>().color = activeColor;
+        w1.equipButton.GetComponent<Image>().color = inactiveColor;
+        w2.equipButton.GetComponent<Image>().color = inactiveColor;
+            
+        Object.Destroy(w1.obj);
+        Object.Destroy(w2.obj);
+            
+        Create(player, -1);
+    }
+
+    private void Create(GameObject player, int id)
+    {
+        obj = Object.Instantiate(prefab, player.transform, true);
+        if (id == 0)
+            obj.GetComponent<Dash>().enabled = false;
+        else if (id == 1)
+            obj.GetComponent<Shield>().enabled = false;
+        var tr = obj.transform;
+        obj.transform.localPosition = Vector3.zero + tr.position;
+        obj.transform.localScale = tr.lossyScale;
+        obj.transform.localRotation = tr.rotation;
+    }
+}
