d231d4df20b3750a83fd4910878a3ef80712476a
Missing Textures <dariormt@gmail.com>
2024-10-03 19:53:49 +0200

    Basic Working Dynamic Enemy

--- Assets/Scripts/DynamicEnemyMovement.cs
+++ Assets/Scripts/DynamicEnemyMovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class DynamicEnemyMovement : MonoBehaviour
 {
+
+    public GameObject bulletSpawner;
+
+    public float speed = 5.0f;
+
+    public bool gotoPlayer;
+    public bool lookAtPlayer;
+
+    private Transform _tr;
+    private Rigidbody2D _rb;
+
+    private GameObject _p;
+
+    private Vector2 _enemyToPlayer;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _tr = transform;
+        _rb = GetComponent<Rigidbody2D>();
+        _p = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        _enemyToPlayer = _p.transform.position - gameObject.transform.position;
+        _enemyToPlayer.Normalize();
         
+        if (gotoPlayer)
+        {
+            _rb.velocity += _enemyToPlayer * (speed * Time.deltaTime);
+        }
+
+        if (lookAtPlayer)
+        {
+            float rot_z = Mathf.Atan2(_enemyToPlayer.y, _enemyToPlayer.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rot_z + 90);
+        }
     }
 }
